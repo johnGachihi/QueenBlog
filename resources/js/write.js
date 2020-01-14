a3e607c973cea89c972415764a4329c5034fb6db
johnGachihi <johngachihi3@gmail.com>
2020-01-14 08:26:02 +0300

    Remove content text-area code

--- resources/js/write.js
+++ resources/js/write.js
@@ -1,25 +1,5 @@
 import BalloonBlockEditor from '@ckeditor/ckeditor5-build-balloon-block'
 
-const contentTextArea = document.getElementById("blog-content-textarea");
-
-contentTextArea.addEventListener('input', e => {
-    autoExpand(e.target)
-}, false);
-
-function autoExpand(field) {
-    field.style.height = 'inherit';
-    const computed = window.getComputedStyle(field);
-
-    var height = parseInt(computed.getPropertyValue('border-top-width'), 10)
-        + parseInt(computed.getPropertyValue('padding-top'), 10)
-        + field.scrollHeight
-        + parseInt(computed.getPropertyValue('padding-bottom'), 10)
-        + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
-
-    field.style.height = height + 'px';
-}
-
-
 const blogContentTextArea = document.getElementById("blog-content-textarea");
 
 BalloonBlockEditor.create(blogContentTextArea)
