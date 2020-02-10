eadb3ee325b89264575a651eb75a4eafff3d9937
johnGachihi <johngachihi3@gmail.com>
2020-02-10 04:56:03 +0300

    Change loop structure for rendering instagram images

--- resources/js/ui/visitors/instagram/InstagramMedia.ts
+++ resources/js/ui/visitors/instagram/InstagramMedia.ts
@@ -6,8 +6,17 @@ const instagramService = new InstagramService(RequestOptionsValues.get());
 
 instagramService.getMedia().then(res => {
     if (res.data !== undefined) {
-        console.log(res.data.entries());
-        for (let [index, media] of res.data.entries()) {
+        console.log(res.data);
+        res.data.foreach((media, index) => {
+            console.log(media);
+            const image = document.createElement('img');
+            image.src = media.media_url;
+            image.alt = 'Instagram image';
+            instagramMediaContainerEl.appendChild(image);
+
+            if (index >=10) throw 'finished'
+        });
+        /*for (let [index, media] of res.data.entries()) {
             console.log(media);
             const image = document.createElement('img');
             image.src = media.media_url;
@@ -15,7 +24,7 @@ instagramService.getMedia().then(res => {
             instagramMediaContainerEl.appendChild(image);
 
             if (index >=10) break;
-        }
+        }*/
     } else if (res.error !== undefined) {
         instagramMediaContainerEl.classList.add('d-none');
         console.log(res.error);
