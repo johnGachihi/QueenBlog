ec0fc94a43650dca8bedceb19093b0ea1d2cba1b
johnGachihi <johngachihi3@gmail.com>
2020-02-19 18:43:20 +0300

    Show authentication error in instagram auth page renee module

--- resources/views/instagram-auth.blade.php
+++ resources/views/instagram-auth.blade.php
@@ -4,6 +4,9 @@
     <div id="instagram-auth-page" class="d-none"></div>
     <span id="instagram-profile-indicator" class="mt-5">Loading profile...</span>
     <div style="height: calc(100vh - 77px)" class="d-flex justify-content-center">
+        @isset($message)
+            <span>{{ $message }}</span>
+        @endisset
         <a href="https://api.instagram.com/oauth/authorize?client_id=732107967315761&redirect_uri=https://unscripted-a-test.wolfteamalpha.ninja/only/juli/instagram-auth&scope=user_profile,user_media&response_type=code"
             class="my-md-auto my-5">
             Give permissions
