eaed8bd8294be5e891141eeba9626b9875899169
johnGachihi <johngachihi3@gmail.com>
2020-02-20 11:57:04 +0300

    Clean up code.
    Remove now unnecessary logs and comments.

--- app/Http/Client/InstagramAuthClient.php
+++ app/Http/Client/InstagramAuthClient.php
@@ -4,24 +4,21 @@
 namespace App\Http\Client;
 
 
+use Exception;
+use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Log;
 
 class InstagramAuthClient
 {
     private $client;
 
-    public function __construct(\GuzzleHttp\Client $client)
+    public function __construct(Client $client)
     {
         $this->client = $client;
     }
 
     public function getShortLivedAccessToken(string $code): string
     {
-        Log::error("getShortLivedAccessToken: " . $code);      // TODO: remove
-        Log::error("getShortLivedAccessToken: " .
-            env('APP_URL') . '/' . env('INSTAGRAM_AUTH_REDIRECT_RELATIVE_URL'));      // TODO: remove
-        Log::error('INSTAGRAM_APP_ID: ' . env('INSTAGRAM_APP_ID'));
-        Log::error('INSTAGRAM_APP_SECRET: ' . env('INSTAGRAM_APP_SECRET'));
         Log::error('request parameters', [
             'client_id' => env('INSTAGRAM_APP_ID'),
             'client_secret' => env('INSTAGRAM_APP_SECRET'),
@@ -47,31 +44,15 @@ class InstagramAuthClient
 
         $response = curl_exec($curl);
         curl_close($curl);
-        Log::error("curl response: $response");
 
-        /*$response = $this->client->request('POST', 'https://api.instagram.com/oauth/access_token', [
-            'headers' => [
-                'Content-Type' => 'application/json'
-            ],
-            'json' => [
-                'client_id' => env('INSTAGRAM_APP_ID'),
-                'client_secret' => env('INSTAGRAM_APP_SECRET'),
-                'grant_type' => 'authorization_code',
-                'redirect_uri' => env('APP_URL') . '/' . env('INSTAGRAM_AUTH_REDIRECT_RELATIVE_URL'),
-                'code' => $code
-            ]
-        ]);*/
-
-//        print_r((array)json_decode($response->getBody()));
         $responseBody = ((array)json_decode($response));
-//        Log::error('responseBody as array:' . $responseBody['error_message']);      // TODO: remove
 
         if (isset($responseBody['access_token'])) { // Request successful
             return ($responseBody['access_token']);
         } else if (isset($responseBody['error_message'])) {
-            throw new \Exception($responseBody['error_message']);
+            throw new Exception($responseBody['error_message']);
         } else {
-            throw new \Exception();
+            throw new Exception();
         }
     }
 
@@ -89,9 +70,9 @@ class InstagramAuthClient
         if (isset($responseBody['access_token'])) {
             return $responseBody['access_token'];
         } else if (isset($responseBody['error_message'])) {
-            throw new \Exception($responseBody['error_message']);
+            throw new Exception($responseBody['error_message']);
         } else {
-            throw new \Exception();
+            throw new Exception();
         }
     }
 }
